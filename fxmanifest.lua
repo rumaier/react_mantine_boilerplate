1bcb9494f1bc8337fe254a33e4363b0613d86a37
rumaier <148654629+rumaier@users.noreply.github.com>
2025-03-19 18:02:07 -0500

    chore: update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,12 @@
  
 fx_version 'cerulean' 
-lua54 'yes' 
-games { 'rdr3', 'gta5' } 
-author 'DirkScripts' 
-description 'React Boilerplate for FiveM | Uses mantine for theming' 
-version      '1.0.5' 
+game 'gta5'
+lua54 'yes'  
+
+name 'r_notify'
+description 'A simple notification system for FiveM'
+author 'r_scripts'
+version '1.0.0'
 
 client_script { 
   'src/client/ui.lua', 
@@ -15,4 +17,10 @@ ui_page 'web/build/index.html'
 files {
 	'web/build/index.html',
 	'web/build/**/*',
+}
+
+escrow_ignore {
+    'install/**/*.*',
+    'locales/*.*',
+    'configs/*.lua'
 }
